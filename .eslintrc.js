01d5eb2b64e49c1f252fe4c0c1bbdfeef26516ba
Tony Klinakis <a.klinakis@yahoo.com>
2021-06-18 02:33:33 +0100

    Add project stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  parser: 'babel-eslint',
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-  },
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: ['react'],
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+	parser: 'babel-eslint',
+	env: {
+		es6: true,
+		node: true,
+		browser: true,
+	},
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: ['react'],
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	rules: {
+		'react/prop-types': 'off',
+	},
 };
