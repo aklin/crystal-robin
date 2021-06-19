ad00a6afd72b98aeb2c5396b25a04fab9d6aeca5
Tony Klinakis <a.klinakis@yahoo.com>
2021-06-19 04:15:40 +0100

    Show ingredient counts

--- src/components/CartDetails/index.js
+++ src/components/CartDetails/index.js
@@ -5,35 +5,64 @@ import CardBody from '../Card/CardBody';
 import CardFooter from '../Card/CardFooter';
 import Button from '@material-ui/core/Button';
 import { Actions } from '../../store/basket';
-import { asArray } from '../../store/coffee';
 import { RemoveShoppingCart } from '@material-ui/icons';
 import CustomTable from '../Table/Table';
 
-export default function CartDetails({ cartState, cartDispatch, coffeeState }) {
-	const itemsCount = Object.keys(cartState).length;
-	const ingredients = asArray(coffeeState)
-		.filter(({ id, type }) => coffeeState[`${type}_${id}`])
-		.map(({ ingredients }) => ingredients);
-
-	const cartRows = Object.keys(cartState)
+const mergeStores = (cartState, coffeeState) =>
+	Object.keys(cartState)
 		.map((uid) => coffeeState[uid])
-		.map(({ title, uid }) => [title, `${cartState[uid]}`]);
+		.sort((a, b) => a.title.localeCompare(b.title))
+		.map(({ uid, ...coffeeItem }) => ({
+			...coffeeItem,
+			uid,
+			inCart: cartState[uid],
+		}));
+
+export default function CartDetails({ cartState, cartDispatch, coffeeState }) {
+	const merged = mergeStores(cartState, coffeeState);
+	const cartEmpty = !Object.keys(cartState).length;
+
+	const cartRows = merged.map(({ title, inCart }) => [title, `${inCart}`]);
+
+	console.log(merged);
+
+	const ingCounts = merged
+		.map(({ ingredients }) => ingredients || [])
+		.reduce((acc, curr) => {
+			curr.forEach(
+				(ingredient) =>
+					(acc[ingredient] = acc[ingredient] ? acc[ingredient] + 1 : 1)
+			);
+			return acc;
+		}, {});
+
+	const ingRows = Object.keys(ingCounts)
+		.sort((a, b) => a.localeCompare(b))
+		.map((key) => [key, `${ingCounts[key]}`]);
 
 	return (
 		<Card>
-			<CardHeader>title</CardHeader>
+			<CardHeader>
+				<h3>Your Cart</h3>
+			</CardHeader>
 			<CardBody>
+				<h4>Items</h4>
 				<CustomTable
 					tableHead={['Item Name', 'Quantity']}
 					tableData={cartRows}
 				/>
 
-				<CustomTable tableHead={['Ingredient', 'Quantity']} tableData={[]} />
+				<h4>Ingredients Breakdown</h4>
+				<p>Adding together all ingredients from the items in your cart</p>
+				<CustomTable
+					tableHead={['Ingredient', 'Quantity']}
+					tableData={ingRows}
+				/>
 			</CardBody>
 			<CardFooter>
 				<span />
 				<Button
-					disabled={!itemsCount}
+					disabled={cartEmpty}
 					onClick={() => cartDispatch({ type: Actions.CLEAR_ALL })}
 				>
 					<RemoveShoppingCart /> Clear Cart
