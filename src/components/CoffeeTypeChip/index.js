93d48c3db9deed76cd03424dc492a76b73c5d616
Tony Klinakis <a.klinakis@yahoo.com>
2021-06-19 06:48:48 +0100

    fix colour

--- src/components/CoffeeTypeChip/index.js
+++ src/components/CoffeeTypeChip/index.js
@@ -3,7 +3,7 @@ import { Chip } from '@material-ui/core';
 import { ColdIcon, HotIcon } from '../AppIcons';
 
 export default function CoffeeTypeChip({ type }) {
-	const colour = type === 'hot' ? 'warning' : 'info';
+	const colour = type === 'hot' ? 'secondary' : 'primary';
 	const icon = type === 'hot' ? <HotIcon /> : <ColdIcon />;
 	return <Chip icon={icon} color={colour} label={type.toUpperCase()} />;
 }
